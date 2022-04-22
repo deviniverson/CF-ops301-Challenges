51eff17e4221f28e4d367ce826c0f37bf7433430
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-04-21 21:19:00 -0700

    update formatting'

--- DateTimeLog.sh
+++ DateTimeLog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Author:                       Devin Iverson
-# Course Code:                       301n2
+# Course Code:                  301n2
 # Script:                       DateTimeLog.sh
 # Purpose:                      Copy system log and rename filename with date and time appended at the end
 
