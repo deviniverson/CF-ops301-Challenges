f9c2ca7abe92dde099794c28fae373b153bad60d
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-04-26 20:05:27 -0700

    finished main script and completed all required objectives

---
+++ Class4:ConditionalsMenuSystem/computerInfo.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Script: computerInfo.sh
+# Author: Devin Iverson
+# Date of latest revision: 04/26/2022
+# Purpose: Use conditionals and loops in Bash Scripting to provide user with computer information. Menu system used to allow user to choose what task is being requested.
+
+# System Menu
+menu(){
+    echo " "
+    echo "        MENU        "
+    echo " "
+    echo "
+    Process_Name Process_Explanation
+    ============ ===================
+    HelloWorld  print_HelloWorld!_on_screen
+    PingSelf  pings_this_computer's_loopback_address
+    IPinfo  prints_network_adapter_information
+    Exit  exit_from_program
+    " | column -t
+    echo " "
+}
+
+
+# Main
+main(){
+    while [ : ]
+    do
+        menu
+        read -p 'Name of process: ' userinput
+
+        if [ $userinput == 'HelloWorld' ] || [ $userinput == 'helloworld' ]
+        then
+            echo "Hello World!"
+            continue
+        
+        elif [ $userinput == 'PingSelf' ] || [ $userinput == 'pingself' ]
+        then
+            ping 127.0.0.1 -c 5
+            continue
+
+        elif [ $userinput == 'IpInfo' ] || [ $userinput == 'ipinfo' ]
+        then
+            echo "$(ifconfig)"
+            continue
+
+        elif [ $userinput == 'Exit' ] || [ $userinput == 'exit' ]
+        then
+            break
+
+        else
+            echo "Input doesn't match an available process. Please try again."
+            continue
+        
+        fi
+    done
+}
+main
