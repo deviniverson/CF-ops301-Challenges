c162218da3c4cdc2d498b9b934caa64500eb2db3
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-05-05 20:03:20 -0700

    filename changed

---
+++ Class7:/DirectoryCreater.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# File Name: DirectoryCreater.py
+# Author: Devin Iverson
+# Date: 05/05/2022
+# Purpose: 
+# 
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+
+### Read user input here into a variable
+
+# Declaration of functions
+
+### Declare a function here
+
+for (root, dirs, files) in os.walk("testdir"):
+    ### Add a print command here to print ==root==
+    print(root)
+    ### Add a print command here to print ==dirs==
+    print(dirs)
+    ### Add a print command here to print ==files==
+    print(files)
+
+# Main
+
+### Pass the variable into the function here
+
+# End
