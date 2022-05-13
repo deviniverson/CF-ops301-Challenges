fa1a97cd8d0803d655cf4d3d0b187d9183555fdd
Devin Iverson <deviniverson@Devins-MBP-19.home>
2022-05-12 20:11:09 -0700

    added stretch goals to script

--- ADNewUser.ps1
+++ ADNewUser.ps1
@@ -3,7 +3,8 @@
 # Date of latest revision: 05/12/2022
 # Purpose: Template to quickly be able to add a user to an Active Directory
 
-$Attributes = @{
+# User attributes
+$User_Attributes = @{
 
     Enabled = $true
     ChangePasswordAtLogon = $true
@@ -25,4 +26,21 @@ $Attributes = @{
     AccountPassword = "FakePassword123" | ConvertTo-SecureString -AsPlainText -Force
 }
 
-New-ADUser @Attributes
+# Groups attributes
+$Group_Attributes = @{
+    
+    Identity = "TPS Group"
+}
+
+# Organization Units attributes
+$OUAttributes = @{
+
+    Name = "TPS Department"
+}
+
+# Add OU command
+New-ADOrganizationalUnit @OUAttributes
+# Add Group command
+New-ADGroup @Group_Attributes
+# Add User command
+New-ADUser @User_Attributes
