a5802172b1e4928bb5b7aae05b7e6af214486563
Devin Iverson <deviniverson@Devins-MacBook-Pro.local>
2022-05-26 21:31:24 -0700

    Class12 started

---
+++ Class12:Psutil/FetchInformation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# File Name: FetchInformation.py
+# Author: Devin Iverson
+# Date: 05/26/2022
+# Purpose: 
+import psutil
+
+cpu_times = psutil.cpu_times()
+
+cpu_stats = psutil.cpu_stats()
+
+
